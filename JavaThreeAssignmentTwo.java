828b85f6741ae34f774acdbbb4409fe0929f4621
MahaPRANAV <MAHALAKSHMI.RAMU@GMAIL.COM>
2017-03-01 18:21:21 +0530

    Add files via upload

---
+++ JavaThreeAssignmentTwo.java
@@ -0,0 +1,21 @@
+
+public class JavaThreeAssignmentTwo {
+	public static void main(String[] args) 
+	{
+		boolean same;
+		String s1 = "Pranadh";
+		String s2 = "Pranav";
+		
+		if (s1 == s2) {
+			
+			same = true ;
+			
+		} 
+		else 
+		{
+			same = false ;
+		}
+   System.out.println(same);				
+	}
+
+}
